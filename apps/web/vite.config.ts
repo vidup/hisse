821d071b0bde1f76942e0e625226453f1176c323
Victor Dupuy <vdcd120491@gmail.com>
2026-03-20 21:39:22 +0100

    add shadcn theme & tailwind to app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
@@ -10,4 +11,7 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    tailwindcss(),
+  ],
 });
